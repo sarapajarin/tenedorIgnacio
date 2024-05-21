39573358eefffe277f5bb289564d8e2fce267ccf
sara <sarapajarin001@gmail.com>
2024-05-21 12:48:25 +0200

    paquetes + funcion

--- printCubierto.js
+++ printCubierto.js
@@ -1,15 +1,61 @@
-console.log("l  l  l")
-console.log("l  l  l")
-console.log("l  l  l")
-console.log("l  l  l")
-console.log("l__l__l")
-console.log("l  l  l")
-console.log("l  l  l")
-console.log("l  l  l")
-console.log("l  l  l")
-console.log("l  l  l")
-console.log("l  l  l")
-console.log("l  l  l")
-console.log("l  l  l")
-console.log("l  I  l")
 
+
+const input = require("prompt-sync")()
+
+function ImprimirCubiertos (nombre){
+    var Cuchillo = "Cuchillo"
+    var Tenedor = "Tenedor"
+    var Cuchara = "Cuchara"
+    if(nombre==Cuchillo){
+        console.log("___    ")
+        console.log("l  l   ")
+        console.log("l  l   ")
+        console.log("l  l   ")
+        console.log("l  l   ")
+        console.log("l__l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+            } else if (nombre ==Tenedor){
+        console.log("l  l  l")
+        console.log("l  l  l")
+        console.log("l  l  l")
+        console.log("l  l  l")
+        console.log("l__l__l")
+        console.log("   l  ")
+        console.log("   l  ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+            } else if (nombre == Cuchara){
+        console.log("_______")      
+        console.log("l     l")
+        console.log("l     l")
+        console.log("l     l")
+        console.log("l     l")
+        console.log("l_____l")
+        console.log("   l  ")
+        console.log("   l  ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")
+        console.log("   l   ")                
+            } 
+
+let cubiertos = ["Cuchara", "Tenedor", "Cuchillo", "Cuchistola"]
+}
+
+var nombre = prompt("Qué cubierto quieres")
